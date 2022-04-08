45c7db339d99de6fd1ca24200261c2a75b0d47eb
michaelstanfa <stanfa.michael@gmail.com>
2022-04-08 12:34:27 -0500

    adding a bunch of stuff to make it work

--- cubs/cubs-lineup.py
+++ cubs/cubs-lineup.py
@@ -3,13 +3,16 @@ import requests
 import os
 import datetime
 import base64
+import json
+from dateutil import tz, parser
+import sys
 
 class CubsLineup:
     def __init__(self):
         self.game_id_list = []
         self.season = "2022-regular"
         self.date = datetime.datetime.now().strftime("%Y%m%d")
-        pass
+
 
     def run(self):
 
@@ -17,7 +20,11 @@ class CubsLineup:
         resp = requests.get(
             url=f"https://api.mysportsfeeds.com/v2.1/pull/mlb/{self.season}/date/{self.date}/games.json?team=CHC",
             headers={'Authorization': f'Basic {basic_token.decode("ascii")}'})
-        games = resp.json().get('games')
+        try:
+            games = resp.json().get('games')
+        except:
+            print("Cannot get game info")
+            sys.exit(1)
         
         for g in games:
             self.game_id_list.append(g.get('schedule').get('id'))
@@ -27,9 +34,12 @@ class CubsLineup:
                 url=f"https://api.mysportsfeeds.com/v2.1/pull/mlb/2022-regular/games/{id}/lineup.json",
                 headers={'Authorization': f'Basic {basic_token.decode("ascii")}'})
             
-            away_lineup = resp.json().get('teamLineups')[0]
-            home_lineup = resp.json().get('teamLineups')[1]
+            response = resp.json()
+            away_lineup = response.get('teamLineups')[0]
+            home_lineup = response.get('teamLineups')[1]
 
+            cubs_home_away = "home" if response.get('game').get('homeTeam').get('abbreviation') == 'CHC' else "away"
+            opponnent_abbr = response.get('game').get('awayTeam').get('abbreviation') if response.get('game').get('homeTeam').get('abbreviation') == "CHC" else response.get('game').get('homeTeam').get('abbreviation')
                         
             away_lineup_positions = away_lineup.get('expected').get('lineupPositions')
             
@@ -45,13 +55,11 @@ class CubsLineup:
             away_starters[8] = [b for b in away_lineup_starters if b.get('position') == 'BO8']
             away_starters[9] = [b for b in away_lineup_starters if b.get('position') == 'BO9']
             away_starters[0] = [b for b in away_lineup_starters if b.get('position') == 'P']
+            home_lineup_positions = home_lineup.get('expected').get('lineupPositions')      
 
-            print(away_starters)
-
-            home_lineup_positions = home_lineup.get('expected').get('lineupPositions')
-            
             home_lineup_starters = [p for p in home_lineup_positions if p.get('position').startswith('B') or p.get('position') == 'P']
             home_starters = {}
+            home_starters[0] = [b for b in home_lineup_starters if b.get('position') == 'P']
             home_starters[1] = [b for b in home_lineup_starters if b.get('position') == 'BO1']
             home_starters[2] = [b for b in home_lineup_starters if b.get('position') == 'BO2']
             home_starters[3] = [b for b in home_lineup_starters if b.get('position') == 'BO3']
@@ -61,15 +69,84 @@ class CubsLineup:
             home_starters[7] = [b for b in home_lineup_starters if b.get('position') == 'BO7']
             home_starters[8] = [b for b in home_lineup_starters if b.get('position') == 'BO8']
             home_starters[9] = [b for b in home_lineup_starters if b.get('position') == 'BO9']        
-            home_starters[0] = [b for b in home_lineup_starters if b.get('position') == 'P']
 
-            print(home_starters)
+            if cubs_home_away == 'home':
+                cubs_lineup = home_starters.copy()
+                cubs_lineup['opposing_sp'] = away_starters[0]
 
-            post_headers = {"muteHttpExceptions": True}
-            print(away_starters)
-            print(home_starters)
+            else:
+                cubs_lineup = away_starters.copy()
+                cubs_lineup['opposing_sp'] = home_starters[0]
+    
+            discord_fields = []
 
+            time = parser.parse(response.get('game').get('startTime'))
+            from_tz = tz.gettz('UTC')
+            to_tz = tz.gettz('America/Chicago')
 
+            central = time.replace(tzinfo=from_tz).astimezone(to_tz).strftime("%I:%m %p")            
+
+            discord_fields.append(
+                {
+                    "name": "Game Time",
+                    "value": f"{datetime.datetime.now().strftime('%A, %B %d, %Y')} at {central}"
+                }
+            )
+
+            discord_fields.append(
+                {
+                    "name": f"Cubs SP",
+                    "value": f"{cubs_lineup[0][0].get('player').get('firstName')} {cubs_lineup[0][0].get('player').get('lastName')}",
+                    "inline": True
+                }
+            )
+
+            discord_fields.append(
+                {
+                    "name": f"{opponnent_abbr} SP",
+                    "value": f"{cubs_lineup['opposing_sp'][0].get('player').get('firstName')} {cubs_lineup['opposing_sp'][0].get('player').get('lastName')}",
+                    "inline": True
+                }
+            )
+            cubs_lineup.pop('opposing_sp')
+            cubs_lineup.pop(0)
+            good_cubs_lineup = {}
+            for item in cubs_lineup:
+                good_cubs_lineup[item] = cubs_lineup[item][0]
+
+            for index in good_cubs_lineup:
+
+                player = good_cubs_lineup.get(index)
+                discord_fields.append(
+                    {
+                        "name": index if index != 0 else "Cubs SP",
+                        "value": f"{player.get('player').get('position')} | {player.get('player').get('firstName')} {player.get('player').get('lastName')}" 
+                    }
+                )
+
+            discord_payload = {
+                "username": "Lineup-Bot",
+                "avatar_url": "https://www.kindpng.com/picc/m/121-1214433_transparent-cubs-clipart-chicago-cubs-svg-free-hd.png",
+                "embeds": [
+                    {
+                        "title": "Cubs Lineup",
+                        "color": "003399",
+                        "fields": discord_fields,
+                        "footer": {
+                            "text": "built using https://birdie0.github.io/discord-webhooks-guide/discord_webhook.html"
+                        }
+                    }
+                ]
+            }
+
+            self.send_to_discord(discord_payload)
+
+            
+
+    def send_to_discord(self, payload):
+        header = {"Content-Type": "application/json"}
+        resp = requests.post(url = os.getenv("DISCORD_CUBS_LINEUP_WEBHOOK_URL"), headers = header, data=json.dumps(payload))
+        resp.raise_for_status
 
 if __name__ == "__main__":
     CubsLineup().run()
